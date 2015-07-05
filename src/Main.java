44f33e51ac4348c5b56c8f19d1ee6af776236994
Cayke <cayke10@gmail.com>
2015-07-04 22:15:47 -0300

    Criacao da classe de gerencia de memoria: - singleton - mapa de bits - metodo de search first fit - metodo para alocao - metodo para desalocar

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ public class Main {
 	static GerenciadorDeProcessos gerenciadorProcessos = new GerenciadorDeProcessos();
 	
 	public static void main(String[] args) throws Exception {
-		gerenciadorProcessos.leArquivoComProcessos();
-		//gerenciadorProcessos.imprimeProcessos();
-		gerenciadorProcessos.loopPrincipal();
+		//gerenciadorProcessos.leArquivoComProcessos();
+		////gerenciadorProcessos.imprimeProcessos();
+		//gerenciadorProcessos.loopPrincipal();
 		
+		
+		//teste		
+		GerenciadorMemoria.getInstance().searchForSpaceInMemory(10);
+		GerenciadorMemoria.getInstance().deallocMemoryWithBaseRegisterAndSize(0, 1);
+		GerenciadorMemoria.getInstance().deallocMemoryWithBaseRegisterAndSize(5, 3);
+		GerenciadorMemoria.getInstance().searchForSpaceInMemory(2);
 	}
 }
