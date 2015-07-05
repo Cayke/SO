646fddc94055e925c1d2aff63b8f73fd44756674
Cayke <cayke10@gmail.com>
2015-07-05 19:24:54 -0300

    criei area de tempo real e de usuario na memoria

--- src/GerenciadorMemoria.java
+++ src/GerenciadorMemoria.java
@@ -2,19 +2,20 @@
 public final class  GerenciadorMemoria {	//classe singleton
 	//instancia que representa a classe
 	private static GerenciadorMemoria instance = null;
-	
+
 	//define size bits map
-	private static final int sizeOfMap = 10;
-	
+	private static final int sizeOfMap = 20;
+	private static final int sizeOfRealTime = 5;
+
 	//mapa de bits
 	private boolean[] bitsMap;
-	
+
 	private GerenciadorMemoria() {
-	      //criar mapa de bits X posicoes
+		//criar mapa de bits X posicoes
 		bitsMap = new boolean[sizeOfMap];
-		
-	   }
-	
+
+	}
+
 	public static GerenciadorMemoria getInstance()
 	{
 		if (instance == null)
@@ -23,7 +24,7 @@ public final class  GerenciadorMemoria {	//classe singleton
 		}
 		return instance;
 	}
-	
+
 	//printa valores do mapa de bits
 	public void printMap ()
 	{
@@ -33,20 +34,20 @@ public final class  GerenciadorMemoria {	//classe singleton
 			System.out.println ("pos[" + i + "] = " + bitsMap[i]);
 		}
 	}
-	
+
 	//procura um espaco de valor X de bits na memoria, caso nao haja espaco retorna -1, caso haja retorna o endereco base e aloca
 	// uso do algoritmo First Fit
-	public int searchForSpaceInMemory(int sizeNeeded)
+	public int searchForSpaceRealTimeMemory(int sizeNeeded)
 	{
 		int baseRegister = -1;
 		int freeSpaces = 0;
-		
-		for (int i = 0; i < sizeOfMap; i++)
+
+		for (int i = 0; i < sizeOfRealTime; i++)
 		{
 			if (bitsMap[i] == false) // posicao vazia
 			{
 				freeSpaces++;
-				
+
 				if (freeSpaces < sizeNeeded && baseRegister == -1) //achou primeiro espaco livre nessa busca e tamanho ainda nao cabe, seta base register e continua
 				{
 					baseRegister = i;
@@ -58,11 +59,11 @@ public final class  GerenciadorMemoria {	//classe singleton
 				else if (freeSpaces == sizeNeeded && baseRegister == -1) //achou primeiro espaco livre nessa busca e processo so precisa de 1 espaco
 				{
 					baseRegister = i;
-					
+
 					//aloca e retorna baseRegister
 					return this.allocMemoryWithBaseRegisterAndSize(baseRegister, sizeNeeded);
 				}
-				
+
 				else if (freeSpaces == sizeNeeded && baseRegister != -1) //espacos continuos >= tamanho necessario para alocao
 				{
 					//aloca e retorna baseRegister
@@ -74,7 +75,7 @@ public final class  GerenciadorMemoria {	//classe singleton
 					//nao faz nada
 				}
 			}
-			
+
 			else //posicao preenchida 
 			{
 				//reseta os valores e continua a procurar
@@ -82,15 +83,72 @@ public final class  GerenciadorMemoria {	//classe singleton
 				freeSpaces = 0;
 			}
 		}
-		
+
 
 		//apagar
 		System.out.println("Falha ao alocar. Nao ha espaco.");
-		
+
 		//nao achou espaco
 		return -1;
 	}
-	
+
+	//procura um espaco de valor X de bits na memoria, caso nao haja espaco retorna -1, caso haja retorna o endereco base e aloca
+	// uso do algoritmo First Fit
+	public int searchForSpaceInUserMemory(int sizeNeeded)
+	{
+		int baseRegister = -1;
+		int freeSpaces = 0;
+
+		for (int i = sizeOfRealTime; i < sizeOfMap; i++)
+		{
+			if (bitsMap[i] == false) // posicao vazia
+			{
+				freeSpaces++;
+
+				if (freeSpaces < sizeNeeded && baseRegister == -1) //achou primeiro espaco livre nessa busca e tamanho ainda nao cabe, seta base register e continua
+				{
+					baseRegister = i;
+				}
+				else if (freeSpaces < sizeNeeded && baseRegister != -1) //ja setou baseRegister e ainda nao cabe
+				{
+					//nao faz nada, so continua procurando
+				}
+				else if (freeSpaces == sizeNeeded && baseRegister == -1) //achou primeiro espaco livre nessa busca e processo so precisa de 1 espaco
+				{
+					baseRegister = i;
+
+					//aloca e retorna baseRegister
+					return this.allocMemoryWithBaseRegisterAndSize(baseRegister, sizeNeeded);
+				}
+
+				else if (freeSpaces == sizeNeeded && baseRegister != -1) //espacos continuos >= tamanho necessario para alocao
+				{
+					//aloca e retorna baseRegister
+					return this.allocMemoryWithBaseRegisterAndSize(baseRegister, sizeNeeded);
+				}
+				else 
+				{
+					//freeEspaces < sizeNeeded  
+					//nao faz nada
+				}
+			}
+
+			else //posicao preenchida 
+			{
+				//reseta os valores e continua a procurar
+				baseRegister = -1;
+				freeSpaces = 0;
+			}
+		}
+
+
+		//apagar
+		System.out.println("Falha ao alocar. Nao ha espaco.");
+
+		//nao achou espaco
+		return -1;
+	}
+
 	private int allocMemoryWithBaseRegisterAndSize(int baseRegister, int size)
 	{
 		//apenas para aumentar robustez do sistema confere se realmente esta vazio esse espaco pedido
@@ -101,20 +159,20 @@ public final class  GerenciadorMemoria {	//classe singleton
 				return -1;
 			}
 		}
-		
+
 		//alocar o espaco
 		for (int i = baseRegister; i < baseRegister + size; i++)
 		{
 			bitsMap[i] = true;
 		}
-		
+
 		//apagar
 		System.out.println("Alocou com sucesso. Mapa de bits:");
 		GerenciadorMemoria.getInstance().printMap();
-		
+
 		return baseRegister;
 	}
-	
+
 	public void deallocMemoryWithBaseRegisterAndSize(int baseRegister, int size)
 	{
 		//desalocar o espaco
@@ -122,11 +180,11 @@ public final class  GerenciadorMemoria {	//classe singleton
 		{
 			bitsMap[i] = false;
 		}
-		
+
 		//apagar
 		System.out.println("Desalocou com sucesso. Mapa de bits:");
 		GerenciadorMemoria.getInstance().printMap();
 	}
-	
-	
+
+
 }
